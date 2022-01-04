e3b031a604e3d2ec080f880f1d981c5b0243dd9f
Cortez Christian <CortezChristian@rocketmortgage.com>
2022-01-04 16:32:59 -0500

    Card lab with new deck, new hand, and reshuffle actions

---
+++ DevBuildDeckOfCardsAPILab/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using DevBuildDeckOfCardsAPILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuildDeckOfCardsAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        DeckDAL DeckDAL = new DeckDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deck model = new Deck();
+            return View(model);
+        }
+
+        public IActionResult  NewDeck(Deck model)
+        {
+            model = DeckDAL.GetDeck();
+            //model = DeckDAL.DrawCards(model.deck_id, 5);
+            return RedirectToAction("DrawCards", model);
+        }
+
+        public IActionResult DrawCards(Deck model)
+        {
+            model = DeckDAL.DrawCards(model.deck_id, 5);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ReshuffleDeck(string deckId)
+        {
+            Deck model = DeckDAL.Reshuffle(deckId);
+            return RedirectToAction("DrawCards", model);
+        }
+
+        [HttpPost]
+        public IActionResult DrawCards(string deckId)
+        {
+            Deck model = DeckDAL.DrawCards(deckId, 5);
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DevBuildDeckOfCardsAPILab/Models/DeckDAL.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DevBuildDeckOfCardsAPILab.Models
+{
+    public class DeckDAL
+    {
+        public Deck GetDeck()
+        {
+            //First step is to put in your Endpoint, your URL 
+            string url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+            //Next we need to create our request 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //Next if your API needs any kind of login or key, that may go here 
+            //SWAPI doesn't need anything so we're good
+
+            //Now we're ready to send off our request and grab the server's response 
+            //Inside our response, the resulting data lives 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull the result into a stream reader which will then give us a string 
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //Grab our response string - read to end starts at the top of our response file and returns each line 
+            //until it hits the end. 
+            string result = rd.ReadToEnd();
+            rd.Close();
+
+            //This line converts our JSON string into a person object automatically 
+            Deck d = JsonConvert.DeserializeObject<Deck>(result);
+
+            //Later we'll convert our string into a model which makes it much easier to use for .net
+            return d;
+        }
+
+        public Deck DrawCards(string deckID, int count)
+        {
+            //First step is to put in your Endpoint, your URL 
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count={count}";
+
+            //Next we need to create our request 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //Next if your API needs any kind of login or key, that may go here 
+            //SWAPI doesn't need anything so we're good
+
+            //Now we're ready to send off our request and grab the server's response 
+            //Inside our response, the resulting data lives 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull the result into a stream reader which will then give us a string 
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //Grab our response string - read to end starts at the top of our response file and returns each line 
+            //until it hits the end. 
+            string result = rd.ReadToEnd();
+            rd.Close();
+
+            //This line converts our JSON string into a person object automatically 
+            Deck d = JsonConvert.DeserializeObject<Deck>(result);
+
+            //Later we'll convert our string into a model which makes it much easier to use for .net
+            return d;
+        }
+
+        public Deck Reshuffle(string deckID)
+        {
+            //First step is to put in your Endpoint, your URL 
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/shuffle/";
+
+            //Next we need to create our request 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //Next if your API needs any kind of login or key, that may go here 
+            //SWAPI doesn't need anything so we're good
+
+            //Now we're ready to send off our request and grab the server's response 
+            //Inside our response, the resulting data lives 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull the result into a stream reader which will then give us a string 
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //Grab our response string - read to end starts at the top of our response file and returns each line 
+            //until it hits the end. 
+            string result = rd.ReadToEnd();
+            rd.Close();
+
+            //This line converts our JSON string into a person object automatically 
+            Deck d = JsonConvert.DeserializeObject<Deck>(result);
+
+            //Later we'll convert our string into a model which makes it much easier to use for .net
+            return d;
+        }
+    }
+}
